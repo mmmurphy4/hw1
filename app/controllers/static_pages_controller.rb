befe58e6bbd5a025a9e66652aca5b79fb53c9358
Michael Murphy <mmmurphy4@hotmail.com>
2014-10-04 18:00:26 -0700

    Done

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,7 @@
 class StaticPagesController < ApplicationController
+
+  def about
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   #get '/about', to: 'CONTROLLER#FUNCTION'
   #get '/user/new', to: 'CONTROLLER#FUNCTION', as: 'users'
-
-
+  root 'users#index'
+  get '/about',to:'static_pages#about'
+  get '/user/new', to: 'users#new', as: 'users'
+  delete '/user/:id', to: 'user#destroy'
 
   #Edit above this line
   get '/user/:id', to: 'users#show', as: 'user'
